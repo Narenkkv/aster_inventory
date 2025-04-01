1e04babd2dbdfc2e58e56ee36acfd4c26ff9b50b
narenkkv97 <narenkkv97@gmail.com>
2025-04-01 18:16:45 +0530

    store name added in layoutpage

--- services/default_data.py
+++ services/default_data.py
@@ -1,19 +1,24 @@
 import os
-from services.models import Usermaster
+from services.models import Usermaster,StoreMaster
 from django.contrib import messages
 
 def inital_data(request):
     try:
         name = None
         isdownload = None
+        storeName = None
         if 'username' in request.session:
             session_username = request.session['username']
             data =  Usermaster.objects.filter(name=session_username).get()
+            if 'storeid' in request.session:
+                getstorename = StoreMaster.objects.get(d_365_store_id = request.session['storeid'])
+                storeName = getstorename.d_365_store_id+'_'+getstorename.store_name_as_per_d_365
             name = data.name
             isdownload = data.isdownload
         return dict(
         name = name,
         isdownload = isdownload,
+        storeName = storeName
     ) 
     except Exception as e:
         print(f"Error in onload function: {e}")
